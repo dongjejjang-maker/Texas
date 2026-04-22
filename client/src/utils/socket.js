f0734b7378e0b927bec1d641847338e98df1e46a
dongjejjang-maker <dongjejjang@gmail.com>
2026-04-22 22:50:39 +0900

    Fix: Resolved room join issue by ensuring roomId type consistency and dynamic socket URL

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
+import { SERVER_URL } from './constants';
 
-const socket = io('https://Sphere-Tinsmith-Thickness.ngrok-free.dev', { 
+const socket = io(SERVER_URL, { 
   path: '/socket.io/', 
   transports: ['websocket', 'polling'] 
 });
