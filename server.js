1e89b43dbb8cabbbf14b506cd576e7c0e139b6ab
dongjejjang-maker <dongjejjang@gmail.com>
2026-04-27 19:53:21 +0900

    Admin: Trigger manual server restart and redeploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+// 🔄 Manual Restart Trigger: 2026-04-27
 const mongoose = require('mongoose'); // 🍏 DB 연동용
 const Hand = require('pokersolver').Hand;
 
