aa2325c9453d2808c99b95452652edb53e1310cb
dongjejjang-maker <dongjejjang@gmail.com>
2026-04-28 21:03:13 +0900

    Fix: Force cache-busting to apply new action sounds

--- server.js
+++ server.js
@@ -167,6 +167,11 @@ console.log('---------------------------');
 app.get('/', (req, res) => {
     const indexPath = path.join(DIST_PATH, 'index.html');
     if (fs.existsSync(indexPath)) {
+        // 🍏 캐시 방지 헤더 추가
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
+        res.setHeader('Surrogate-Control', 'no-store');
         res.sendFile(indexPath);
     } else {
         res.status(404).send('Frontend build not found. Please check deployment logs.');
